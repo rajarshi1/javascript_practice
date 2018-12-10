aed3162c4d82a6a0be4d4bd95d8d8fc8c64ee3a9
Rajarshi <ghoshal.rajarshi@gmail.com>
2018-12-10 22:07:25 +0530

    Day 14

--- LodashKiller/compact.js
+++ LodashKiller/compact.js
@@ -2,3 +2,18 @@ export default function compact(arr) {
 	let newArray = arr.filter(i => {if(i) return true;})
 	return newArray;
 }
+
+// function compact(arr) {
+
+//     let newArray = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i]==false || null || 0 || "" || undefined || NaN ){
+//             arr.splice(i,0);
+//         }
+//         else{
+//             newArray.push(arr[i]);
+//         }
+//     }
+//     return newArray;  
+// }
+// compact([1,"",1]);
